43aa65352fc5345a254e774e6df4ccd8b8e5bd37
ahmednour <ahmednour1430464@gmail.com>
2021-06-23 16:31:50 +0200

    configure main store for users requests and coaches

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,28 @@
-import { createStore } from 'vuex'
+import { RequestState, State, UserState } from "@/@types";
+import { createStore } from "vuex";
+import { store as coach, Store as CoachStore } from "./modules/coaches";
+import { store as request, Store as RequestStore } from "./modules/requests";
+import { store as user, Store as UserStore } from "./modules/users";
 
-export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+export type RootState = {
+  coaches: State;
+  requests: RequestState;
+  users: UserState;
+};
+export type Store = CoachStore<Pick<RootState, "coaches">> &
+  RequestStore<Pick<RootState, "requests">> &
+  UserStore<Pick<RootState, "users">>;
+
+const store = createStore<RootState>({
   modules: {
-  }
-})
+    coach,
+    request,
+    user,
+  },
+});
+
+export const useStore = (): Store => {
+  return store as Store;
+};
+
+export default store;
