13e62d9638b9648543678de8db3a5e83a246eaa8
ahmednour <ahmednour1430464@gmail.com>
2021-06-23 16:22:57 +0200

    manage routing inside vue

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,77 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
-
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import Login from "@/views/users/Login.vue";
+import Signup from "@/views/users/Signup.vue";
+import CoachDetails from "@/views/coaches/CoachDetails.vue";
+import CoachesList from "@/views/coaches/CoachesList.vue";
+import CoachRegistration from "@/views/coaches/CoachRegistration.vue";
+import ContactCoach from "@/views/requests/ContactCoach.vue";
+import RequestsReceived from "@/views/requests/RequestsReceived.vue";
+import NotFound from "@/views/NotFound.vue";
+import { routerEnum } from "@/lib/Enums";
+import { AuthMiddleWare, LogMiddleWare } from "./middlewares";
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: routerEnum.LOGIN,
+    component: Login,
+    name:routerEnum.LOGIN,
+    beforeEnter:LogMiddleWare
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: routerEnum.SIGNUP,
+    component: Signup,
+    name:routerEnum.SIGNUP,
+    beforeEnter:LogMiddleWare
+  },
+  {
+    path: routerEnum.HOME,
+    name: routerEnum.HOME,
+    redirect: routerEnum.COACHES,
+  },
+  {
+    path: routerEnum.COACHES,
+    component: CoachesList,
+    name:routerEnum.COACHES,
+    beforeEnter:AuthMiddleWare
+  },
+  {
+    path: routerEnum.COACH,
+    component: CoachDetails,
+    name:routerEnum.COACH,
+    props: true,
+    beforeEnter:AuthMiddleWare,
+    children: [
+      {
+        path: routerEnum.CONTACT,
+        component: ContactCoach,
+        name:routerEnum.CONTACT,
+        props: true,
+        beforeEnter:AuthMiddleWare
+      },
+    ],
+  },
+  {
+    path: routerEnum.REGISTER,
+    component: CoachRegistration,
+    name:routerEnum.REGISTER,
+    beforeEnter:AuthMiddleWare
+  },
+  {
+    path: routerEnum.REQUESTS,
+    component: RequestsReceived,
+    name:routerEnum.REQUESTS,
+    beforeEnter:AuthMiddleWare
+  },
+  {
+    path: routerEnum.NOTFOUND,
+    component: NotFound,
+    name:routerEnum.NOTFOUND,
+    beforeEnter:AuthMiddleWare
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
