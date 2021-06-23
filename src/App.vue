c99fc8c3d1eae85bee73890fadc6c3c2fee16613
ahmednour <ahmednour1430464@gmail.com>
2021-06-23 17:45:38 +0200

    set router view and manage router links

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <the-header></the-header>
+  <router-view />
 </template>
 
+<script lang="ts">
+import TheHeader from "@/components/layout/TheHeader.vue";
+import { defineComponent } from "@vue/runtime-core";
+import { UserActionTypes } from "./lib/Enums";
+import { IUser } from "./lib/Interfaces";
+export default defineComponent({
+  components: {
+    TheHeader,
+  },
+});
+</script>
+
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+* {
+  box-sizing: border-box;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+html {
+  font-family: "Roboto", sans-serif;
+}
+body {
+  margin: 0;
 }
 </style>
