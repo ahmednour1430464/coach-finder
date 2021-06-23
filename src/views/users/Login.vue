645c68c4c68ad01f2e3005b5e35a30288951aa6c
ahmednour <ahmednour1430464@gmail.com>
2021-06-23 17:46:31 +0200

    simple login system

---
+++ src/views/users/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>login</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { IUser } from "@/lib/Interfaces";
+import { UserActionTypes } from "@/lib/Enums";
+export default defineComponent({
+  name: "login",
+  created() {
+    const email = prompt("please use email to get into this app");
+    const user:IUser={
+
+      email:<string>email,
+    }
+    this.$store.dispatch(UserActionTypes.SET_USER, user);
+  },
+});
+</script>
+
+
---
+++ src/views/users/Signup.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>singup</div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name:'signup'
+});
+</script>
+
+
