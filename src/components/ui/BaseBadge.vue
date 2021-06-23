977c5cca09b3d9631ccb373b52b16199bb8a1311
ahmednour <ahmednour1430464@gmail.com>
2021-06-23 16:28:01 +0200

    base components for entire project

---
+++ src/components/ui/BaseBadge.vue
@@ -0,0 +1,52 @@
+<template>
+  <span class="badge" :class="type">
+    {{ text }}
+  </span>
+</template>
+
+
+<script lang="ts">
+export default {
+  props: {
+    title: {
+      type: String,
+      require:true
+    },
+    type: {
+      type: String,
+      require:true
+    },
+  },
+  computed: {
+    text(this:{title:string}): string {
+     return this.title.toUpperCase()
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.badge {
+  background-color: #ccc;
+  color: #252525;
+  border-radius: 30px;
+  padding: 0.5rem 1.5rem;
+  display: inline-block;
+  margin-right: 0.5rem;
+}
+
+.frontend {
+  background-color: #3d008d;
+  color: white;
+}
+
+.backend {
+  background-color: #71008d;
+  color: white;
+}
+
+.career {
+  background-color: #8d006e;
+  color: white;
+}
+</style>
---
+++ src/components/ui/BaseButton.vue
@@ -0,0 +1,76 @@
+<template>
+  <router-link v-if="link" :to="to" :class="mode">
+    <slot></slot>
+  </router-link>
+  <button v-else :class="mode">
+    <slot></slot>
+  </button>
+</template>
+<script lang="ts">
+import { Mode } from "@/lib/Enums";
+import { defineComponent, PropType } from "@vue/runtime-core";
+export default defineComponent({
+  props: {
+    link: {
+      type: Boolean,
+      require: true,
+      default: false,
+    },
+    to: {
+      type: String,
+      require: true,
+      default: "/",
+    },
+    mode: {
+      type: String as PropType<Mode>,
+      require: true,
+      default: Mode.FLAT,
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+$background: #3a0061;
+$active: #270041;
+button,
+a {
+  text-decoration: none;
+  padding: 0.75rem 1.5rem;
+  font: inherit;
+  background-color: $background;
+  border: 1px solid $background;
+  color: white;
+  cursor: pointer;
+  border-radius: 30px;
+  margin-right: 0.5rem;
+  display: inline-block;
+}
+
+a:hover,
+a:active,
+button:hover,
+button:active {
+  background-color: $active;
+  border-color: $active;
+}
+
+.flat {
+  border: none;
+}
+
+.outline {
+  background-color: transparent;
+  border-color: $active;
+  color: $active;
+}
+
+.flat:hover,
+.flat:active {
+  background-color: $active;
+}
+.outline:hover,
+.outline:active {
+  background-color: #edd2ff;
+}
+</style>
